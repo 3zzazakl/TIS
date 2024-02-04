82ab329f47be260f88de08ad5546db3f914274c5
3zzazakl <vanzekovichgr@gmail.com>
2024-02-05 00:08:13 +0200

    best time for stocks

---
+++ 0x00-v/0x31-Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    BigO: O(n)
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) == 0:
+            return (0)
+
+        min_price = prices[0]
+        max_profit = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                max_profit = max(max_profit, (prices[i] - min_price))
+        return (max_profit)
+
+
+leetcode = Solution()
+prices1 = [7, 1, 5, 3, 6, 4]
+prices2 = [7, 6, 4, 3, 1]
+
+print(leetcode.maxProfit(prices1))
+print(leetcode.maxProfit(prices2))

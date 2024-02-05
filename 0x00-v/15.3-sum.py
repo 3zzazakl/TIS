741963ceaf2b4983fcd483d702f6b313a14dc195
3zzazakl <vanzekovichgr@gmail.com>
2024-02-05 09:12:46 +0200

    solved task15

---
+++ 0x00-v/15.3-sum.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+# BigO: O(n^2)
+# @lc code=start
+from operator import le
+
+
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        result = []
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            
+            while (left < right):
+                total = nums[i] + nums[left] + nums[right]
+                
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    
+                    while (left < right and nums[left] == nums[left + 1]):
+                        left += 1
+                    while (left < right and nums[right] == nums[right - 1]):
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return (result)
+# @lc code=end
+

4efcec2cda3adbc2078642ecb75e7cf962b1cf6d
3zzazakl <vanzekovichgr@gmail.com>
2024-02-06 21:44:44 +0200

    runtime beats 84.46, runtime beats 84.46

---
+++ 0x00-v/88.merge-sorted-array.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+# BugO = O(m + n)
+# @lc code=start
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        ii = n - 1
+        iii = m + n - 1
+        
+        while i >= 0 and ii >= 0:
+            if nums1[i] > nums2[ii]:
+                nums1[iii] = nums1[i]
+                i -= 1
+            else:
+                nums1[iii] = nums2[ii]
+                ii -= 1
+            iii -= 1
+        
+        while ii >= 0:
+            nums1[iii] = nums2[ii]
+            ii -= 1
+            iii -= 1
+# @lc code=end
+

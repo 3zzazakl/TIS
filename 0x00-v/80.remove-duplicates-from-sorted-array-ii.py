16fff35e8df886728749f368ba150ebf93d1fb37
3zzazakl <vanzekovichgr@gmail.com>
2024-02-06 19:11:04 +0200

    setting en

--- 0x00-v/80.remove-duplicates-from-sorted-array-ii.py
+++ 0x00-v/80.remove-duplicates-from-sorted-array-ii.py
@@ -9,7 +9,7 @@ class Solution:
     def removeDuplicates(self, nums):
         if len(nums) <= 2:
             return (len(nums))
-    
+
         index = 2
         for i in range(2, len(nums)):
             if nums[i] != nums[index - 2]:
